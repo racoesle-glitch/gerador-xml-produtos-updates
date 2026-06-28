589e7fa49ccc5ebd0351ac6e092933d36bd9b6ef
racoesle-glitch <racoesle@gmail.com>
2026-06-28 18:46:33 -0300

    Add files via upload

---
+++ updates/app-config.js
@@ -0,0 +1,12 @@
+window.XML_FACIL_CONFIG = {
+  appName: 'XML Facil',
+  appSubtitle: 'Gerador de XML para produtos',
+  logoUrl: 'app-logo.svg',
+  documentTitle: 'XML Facil',
+  heroTitle: 'Controle seus XMLs de importacao sem refazer servico.',
+  heroText: 'Crie documentos, cole listas grandes de produtos, gere XML, salve historico e volte depois para conferir, duplicar ou baixar de novo.',
+  footerLines: [
+    'Versao beta publica',
+    'Desenvolvido por Ronaldo Junio'
+  ]
+};
