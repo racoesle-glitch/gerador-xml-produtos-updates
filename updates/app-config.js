c900f0e73e134a594704e5ef1e5e11ee7ddece83
RONALDO JUNIO.A <racoesle@gmail.com>
2026-06-28 19:58:36 -0300

    Add files via upload

--- updates/app-config.js
+++ updates/app-config.js
@@ -5,8 +5,9 @@ window.XML_FACIL_CONFIG = {
   documentTitle: 'XML Facil',
   heroTitle: 'Controle seus XMLs de importacao sem refazer servico.',
   heroText: 'Crie documentos, cole listas grandes de produtos, gere XML, salve historico e volte depois para conferir, duplicar ou baixar de novo.',
+  supportEmail: 'junioronaldo896@gmail.com',
   footerLines: [
     'Versao beta publica',
-    'Desenvolvido por Ronaldo Junio'
+    'Criado e desenvolvido por Ronaldo Junio'
   ]
 };
